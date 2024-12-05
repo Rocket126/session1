1e31f73f23668c6ca6120c5436aa1b8a5f3c32d9
IniciandoElcambio <carlosgctec@mail.com>
2024-12-04 21:11:58 -0300

    primer commit

---
+++ src/main/java/com/models/Usuario.java
@@ -0,0 +1,11 @@
+package com.models;
+import lombok.*;
+@Data
+@AllArgsConstructor //para obtener el constructor
+@NoArgsConstructor
+public class Usuario {
+
+    private String nombre;
+
+
+}
---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,20 @@
+package org.example;
+
+
+import com.models.Usuario;
+
+
+public class App
+{
+    public static void main( String[] args )
+    {
+
+        Usuario usuario = new Usuario();
+
+        usuario.setNombre("carlos");
+        System.out.println(usuario.getNombre());
+
+        Usuario usuario2 = new Usuario();
+
+    }
+}
